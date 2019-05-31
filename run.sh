5e853a651594cffb71ae3b1fb67cfa694e70c8b8
thr3a <12jk172@ms.dendai.ac.jp>
2019-06-01 03:27:15 +0900

    a

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -v `pwd`:/works -it --rm --runtime=nvidia tahara/keras-sample python works/fruit.py
+docker run -v `pwd`:/works -it --rm --runtime=nvidia tahara/keras-sample python works/sign_language.py
